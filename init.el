3891203e2d824ed9de478f10d5e262a8c571aaac
e_fpischedda <francesco.pischedda@abinsula.com>
2018-11-18 18:17:55 +0100

    add ranger and counsel

--- init.el
+++ init.el
@@ -25,13 +25,20 @@
 ;; This is only needed once, near the top of the file
 (eval-when-compile
   ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  ( add-to-list 'load-path "~/.emacs.d/use-package")
+  (add-to-list 'load-path "~/.emacs.d/use-package")
   (require 'use-package))
 
 
 (use-package zenburn-theme
   :ensure t
   :init (load-theme 'zenburn t))
+
+(use-package ranger :ensure t
+  :commands (ranger)
+  :bind (("C-x d" . deer))
+  :config
+  (setq ranger-cleanup-eagerly t)
+  )
 
 (use-package magit
   :ensure t
@@ -62,7 +69,27 @@
   :config (powerline-default-theme))
 
 (use-package ivy
-  :ensure t)
+  :ensure t
+  :diminish (ivy-mode . "") ; does not display ivy in the modeline
+  :init (ivy-mode 1)        ; enable ivy globally at startup
+  :bind (:map ivy-mode-map  ; bind in the ivy buffer
+         ("C-'" . ivy-avy)) ; C-' to ivy-avy
+  :config
+  (setq ivy-use-virtual-buffers t)   ; extend searching to bookmarks and …
+  (setq ivy-height 20)               ; set height of the ivy window
+  (setq ivy-count-format "(%d/%d) ") ; count format, from the ivy help page
+  )
+
+(use-package counsel :ensure t
+  :bind*                           ; load counsel when pressed
+  (("M-x"     . counsel-M-x)       ; M-x use counsel
+   ("C-x C-f" . counsel-find-file) ; C-x C-f use counsel-find-file
+   ("C-x C-r" . counsel-recentf)   ; search recently edited files
+   ("C-c f"   . counsel-git)       ; search for files in git repo
+   ("C-c s"   . counsel-git-grep)  ; search for regexp in git repo
+   ("C-c /"   . counsel-ag)        ; search for regexp in git repo using ag
+   ("C-c l"   . counsel-locate))   ; search for files or else using locate
+  )
 
 (use-package elpy
   :ensure t
